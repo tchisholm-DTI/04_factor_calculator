95626ae69216c77006694b9c04c69a3a45b96bb4
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-07-01 14:55:17 +1200

    update factor calculator

--- 00_factors_calc.py
+++ 00_factors_calc.py
@@ -56,3 +56,55 @@ def num_check(question):
 
 # Gets factors, returns a sorted list
 
+# Main routine goes here 
+
+# Heading
+statement_generator("Factors Calculator", "-")
+
+# Displays instructions if user has not used the program before
+first_time = input("Press <enter> to see the instructions or any key to continue")
+
+if first_time =="":
+    instructions()
+
+# Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+    
+    comment = ""
+
+    # Ask user for number to be factorised ...
+    var_to_factor = num_check("Number? ")
+
+    if var_to_factor !=1:
+        factor_list = get_factors(var_to_factor)
+
+    else:
+        factor_list = ""
+        comment = "One is UNITY! It only has one factor. Itself :)"
+
+    # Comments for squares/primes
+    if len(factor_list) == 2:
+        comment = "{} is a prime- number. ".format(var_to_factor)
+    elif len(factor_list) % 2 == 1:
+        comment = "{} is a perfect square".format(var_to_factor)
+    
+    # Output factors and comment
+    if var_to_factor == 1:
+        heading = "One is special..."
+
+    else:
+        heading = "Factors of {}".format(var_to_factor)
+    
+    # Output factors and comment
+    statement_generator(heading, "*")
+    print()
+    print(factor_list)
+    print(comment)
+
+    print()
+    keep_going = input(" Press <enter> to continue or any key to quit ")
+    print()
+
+print()
+print("Thank you for using the factors calculator")
