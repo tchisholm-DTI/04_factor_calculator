e3c40f5d094856889a8a747a8543818b74d5a304
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-07-01 12:22:37 +1200

    Update 00_factors_calc.py

--- 00_factors_calc.py
+++ 00_factors_calc.py
@@ -1,4 +1,31 @@
 # Functions go here
 
 # Put series of symbols at start and end of text
+def statement_generator(text, decoration):
+
+    # Make string with five characters
+    ends = decoration * 5
+
+    # Add decoration to start and end of statement
+    statement = "{} {} {}" .format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+
+# Displays instructions/information
+def instructions():
+
+    statement_generator("Instructions/information", "-")
+    print()
+    print("Please choose a data type (image/text/integer)")
+    print()
+    print("This program assumes that images are being represented in 24 bit colour (ie: 24 bits per pixel). For text, we assume that ascii encoding is being used (8 bits per character.) ")
+    print()
+    print("Complete a many calculations as necessary, pressing <enter> at the end of each calculation or any key to quit")
+    print()
+    return ""
 
