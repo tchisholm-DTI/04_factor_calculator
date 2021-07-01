7aea8cc70a0a45911feee90cdf7b9b5d3b6a31fd
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-07-01 12:26:08 +1200

    Update 01_integer_checker.py

--- 01_integer_checker.py
+++ 01_integer_checker.py
@@ -1,16 +1,16 @@
-# Checks input is a number more than zero
+# Checks input is a number more than one
 def num_check(question, low):
     valid = False
     while not valid:
 
-        error = "Please enter an integer that is more than zero" "or (equal to) {}" .format(low)
+        error = "Please enter an integer that is more than one" "or (equal to) {}" .format(low)
 
         try:
 
             # Ask user to enter a number
             response = int(input(question))
 
-            # Checks number is more than zero
+            # Checks number is more than one
             if response >= low:
                 return response
 
@@ -28,11 +28,5 @@ keep_going = ""
 while keep_going == "":
     print()
 
-    # Ask user for an integer (must be more than or equal to 0)
-    var_integer = num_check("Enter an integer:", 0) 
-
-    # Ask user for the width and height of an image 
-    # (must be greater than or equal to 1
-    image_width = num_check("Image Width?:", 1)
-    print()
-    image_height = num_check("Image Height?:", 1)
+    # Ask user for an integer (must be more than or equal to 1)
+    var_integer = num_check("Enter an integer:", 1) 
