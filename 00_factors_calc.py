a9e649b00c891337ff7e54404e201d31a01bfc38
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-07-01 14:30:53 +1200

    Update 00_factors_calc.py

--- 00_factors_calc.py
+++ 00_factors_calc.py
@@ -29,3 +29,30 @@ def instructions():
     print()
     return ""
 
+# Checks input is a number more than a given number
+def num_check(question):
+    valid = False
+    while not valid:
+
+        error = "Please enter a number that is more than (or equal to) one"
+
+        try:
+
+            # Ask user to enter a number
+            response = float(input(question))
+
+            # Checks number is more than (or equal to) one
+            if response >= 1:
+                return response
+
+            # Outputs error if input is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+            print()
+
+# Gets factors, returns a sorted list
+
