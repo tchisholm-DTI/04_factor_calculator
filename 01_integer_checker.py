c08ea83117009f4ea660802a2d4c21392f65b4a7
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-07-01 12:30:08 +1200

    Update 01_integer_checker.py

--- 01_integer_checker.py
+++ 01_integer_checker.py
@@ -1,17 +1,17 @@
 # Checks input is a number more than one
-def num_check(question, low):
+def num_check(question):
     valid = False
     while not valid:
 
-        error = "Please enter an integer that is more than one" "or (equal to) {}" .format(low)
+        error = "Please enter a number that is more than (or equal to) one"
 
         try:
 
             # Ask user to enter a number
-            response = int(input(question))
+            response = float(input(question))
 
-            # Checks number is more than one
-            if response >= low:
+            # Checks number is more than (or equal to) one
+            if response >= 1:
                 return response
 
             # Outputs error if input is invalid
@@ -22,7 +22,6 @@ def num_check(question, low):
         except ValueError:
             print(error)
             print()
-
 # Main routine goes here
 keep_going = ""
 while keep_going == "":
